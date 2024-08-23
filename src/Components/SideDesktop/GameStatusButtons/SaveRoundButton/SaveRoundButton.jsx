17321ee1f6aa429b7eec7828bea8f1ef8ecbcb39
TomekBekus02 <156850117+TomekBekus02@users.noreply.github.com>
2024-08-23 23:55:41 +0200

    still need to fixed some bugs

--- src/Components/SideDesktop/GameStatusButtons/SaveRoundButton/SaveRoundButton.jsx
+++ src/Components/SideDesktop/GameStatusButtons/SaveRoundButton/SaveRoundButton.jsx
@@ -8,37 +8,51 @@ export default function SaveButton(){
 
     async function handleGameProgress(){
         const updatedCheckedColors = [false, false, false, false];
+        const updatePossibleUserCheckedColors = [false, false, false, false];
         console.log(`${updatedCheckedColors.map(item => item)}`);
         const updatedColorsAccuracy = [0,0,0,0];
         let tempAccuracy = 0;
-        let tempJ = 4;
+        let tempAi = 4;
         console.log(`AI answer: ${answer.map(item => item)}`);
         console.log(`user answer: ${fourDecodedColors.map(item => item)}`);
         for(let user = 0; user < 4; user++){
             tempAccuracy = 0;
+            tempAi = 4;
+            if(updatePossibleUserCheckedColors[user]){
+                continue;
+            }
             for(let Ai = 0; Ai < 4; Ai++){
+                console.log(`false/true == ${updatedCheckedColors[Ai]}`);
+                console.log(`[user:${user}][Ai:${Ai}] AI: ${answer[Ai]} user: ${fourDecodedColors[user]}`);
                 if(updatedCheckedColors[Ai] === false && fourDecodedColors[Ai] !== '' && fourDecodedColors[user] === answer[Ai]){
-                    console.log(`[user:${user}][Ai:${Ai}] AI: ${answer[Ai]} user: ${fourDecodedColors[user]}`);
-                    console.log(`false/true == ${updatedCheckedColors[Ai]}`);
                     if(user===Ai){
-                        updatedCheckedColors[Ai] = true
+                        updatedCheckedColors[Ai] = true;
                         tempAccuracy = 2;
                         console.log(`2`);
                         break;
                     }
                     else{
-                        if(fourDecodedColors[Ai]===fourDecodedColors[user]){
-                            updatedCheckedColors[user] = true;
-                            updatedColorsAccuracy[user] = 2;
+                        console.log(`sprawdzenie czy na tym potencjaklnym miejsu jest lepiej pasujacy index z tej petli ${fourDecodedColors[Ai]} index z wyzszej petli ${fourDecodedColors[user]}`);
+                        if(answer[Ai]===fourDecodedColors[Ai]){
+                            updatePossibleUserCheckedColors[Ai] = true;
+                            updatedCheckedColors[Ai] = true;
+                            updatedColorsAccuracy[Ai] = 2;
+                            console.log(`2`);
                             tempAccuracy = 0;
                             continue;
                         }
-                        tempAccuracy = 1;
-                        console.log('1'); 
+                        else if(tempAi===4){
+                            tempAccuracy = 1;
+                            tempAi=Ai;
+                            console.log('1'); 
+                        }
+
                     }   
                 }
             }
-            updatedColorsAccuracy[user] = tempAccuracy;  
+            updatedColorsAccuracy[user] = tempAccuracy;
+            console.log(`user answer: ${updatedColorsAccuracy.map(item => item)}`);
+            tempAi !== 4 ? updatedCheckedColors[tempAi] = true : null;
         }
         updatedColorsAccuracy.sort((a,b)=>b-a);
         await setColorAccuracy([0,0,0,0]);
