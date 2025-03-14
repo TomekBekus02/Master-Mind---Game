720657986804b9677e71780acfb3583ccaf0a78d
TomekBekus02 <156850117+TomekBekus02@users.noreply.github.com>
2025-03-14 03:20:04 +0100

    changed button name

--- src/Components/SideDesktop/GameStatusButtons/StartButton/StartButton.jsx
+++ src/Components/SideDesktop/GameStatusButtons/StartButton/StartButton.jsx
@@ -2,9 +2,9 @@ import { useContext } from 'react'
 import { GameContex } from '../../../source/gameContex'
 import './StartButton.css'
 
-export default function StartButton(){
-    const {handleRound, handleStartGame, addAnswer, colors, startGame, answer} = useContext(GameContex);
-    const randomNumber = () => {return Math.floor(Math.random() * colors.length)};
+export default function StartButton() {
+    const { handleRound, handleStartGame, addAnswer, colors, startGame, answer } = useContext(GameContex);
+    const randomNumber = () => { return Math.floor(Math.random() * colors.length) };
 
     function gameLaunching() {
         //const newAnswers = [];
@@ -13,13 +13,13 @@ export default function StartButton(){
             addAnswer(randomNumber())
         }
         //newAnswers.forEach(answer => addAnswer(answer));
-        handleRound(); 
+        handleRound();
         handleStartGame();
     }
-    return(  
-        <button 
-            onClick={!startGame ? () => gameLaunching() : null} 
+    return (
+        <button
+            onClick={!startGame ? () => gameLaunching() : null}
             className="start-button"
-        >Start Button</button>
+        >Start Game</button>
     )
 }
